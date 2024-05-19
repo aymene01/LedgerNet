ffe5dd5f8bc07dd45884bcefcd6d1fd390042010
aymene01 <aymene01@hotmail.fr>
2024-05-19 22:31:18 +0200

    refacto main and running server

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"net"
 	"time"
 
 	"github.com/aymene01/blocker/node"
@@ -16,18 +14,6 @@ import (
 func main() {
 	node := node.NewNode()
 
-	opts := []grpc.ServerOption{}
-	grpcServer := grpc.NewServer(opts...)
-
-	ln, err := net.Listen("tcp", ":3000")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pb.RegisterNodeServer(grpcServer, node)
-	fmt.Println("node running on port:", "3000")
-
 	go func() {
 		for {
 			time.Sleep(2 * time.Second)
@@ -35,7 +21,7 @@ func main() {
 		}
 	}()
 
-	grpcServer.Serve(ln)
+	log.Fatal(node.Start(":3000"))
 }
 
 func getClient() (*grpc.ClientConn, error) {
--- node/node.go
+++ node/node.go
@@ -2,9 +2,13 @@ package node
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net"
+	"strings"
 
 	"github.com/aymene01/blocker/pb"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/peer"
 )
 
@@ -17,6 +21,29 @@ func NewNode() *Node {
 	return &Node{
 		version: "blocker-1",
 	}
+}
+
+func (n *Node) Start(listenAddr string) error {
+	opts := []grpc.ServerOption{}
+	grpcServer := grpc.NewServer(opts...)
+
+	ln, err := net.Listen("tcp", listenAddr)
+
+	if err != nil {
+		return err
+	}
+
+	pb.RegisterNodeServer(grpcServer, n)
+
+	portNum, err := getPortNum(listenAddr)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("node running on port:", portNum)
+
+	return grpcServer.Serve(ln)
 }
 
 func (n *Node) Handshake(ctx context.Context, v *pb.Version) (*pb.Version, error) {
@@ -36,4 +63,13 @@ func (n *Node) HandleTransaction(ctx context.Context, tx *pb.Transaction) (*pb.A
 	fmt.Println("received tx from:", peer)
 
 	return &pb.Ack{}, nil
+}
+
+func getPortNum(listenAddr string) (string, error) {
+	values := strings.Split(listenAddr, ":")
+	if len(values) != 2 {
+		return "", errors.New("invalid listen value")
+	}
+
+	return values[1], nil
 }
